1117df7dba401db0a30fd8383c092223e8e1d55d
FirozHossain82 <firozhossain295@gmail.com>
2022-12-06 23:49:26 +0600

    footer update

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
+    return ( 
         <div>
             <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
                 <div className="grid grid-flow-col gap-4 font-medium">
